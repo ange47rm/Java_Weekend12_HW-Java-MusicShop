f3d2f3c335d990ac1e3998dab5d4918a683b57c4
ange47rm <ange47rm@hotmail.com>
2021-02-20 13:45:37 +0000

    Added Shop and ShopTests. All 44 tests passing. MVP complete

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,8 +1,31 @@
 package shop;
 
+import behaviours.ISell;
+
 import java.util.ArrayList;
 
 public class Shop {
 
-    private ArrayList
+    private ArrayList<ISell> stock;
+
+    public Shop(){
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public void addItemToStock(ISell item){
+        this.stock.add(item);
+    }
+
+    public void removeItemFromStock(ISell item){
+        int itemToRemove = this.stock.indexOf(item);
+        this.stock.remove(itemToRemove);
+    }
+
+    public ArrayList<ISell> getStock() {
+        return this.stock;
+    }
+
+    public int getItemInStockCount(){
+        return this.stock.size();
+    }
 }
--- src/test/java/shop/ShopTest.java
+++ src/test/java/shop/ShopTest.java
@@ -1,4 +1,46 @@
 package shop;
 
+import instruments.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class ShopTest {
+
+    public Shop shop;
+    public Piano piano;
+    public Trombone trombone;
+    public Accessory accessory;
+
+    @Before
+    public void before(){
+        shop = new Shop();
+        accessory = new Accessory("Guitar Strings x5", 3, 7);
+        piano = new Piano(InstrumentType.PERCUSSION, "Cast iron",  "Black", 1799, 2350, PianoType.UPRIGHT);
+        trombone = new Trombone(InstrumentType.BRASS, "Brass", "Silver", 180, 225, TromboneType.BASE);
+    }
+
+    @Test
+    public void shopHasStock(){
+        assertNotNull(shop.getStock());
+    }
+
+    @Test
+    public void canAddItemToStock(){
+        shop.addItemToStock(piano);
+        shop.addItemToStock(accessory);
+        assertEquals(2, shop.getItemInStockCount());
+    }
+
+    @Test
+    public void canRemoveItemFromStock(){
+        shop.addItemToStock(piano);
+        shop.addItemToStock(accessory);
+        shop.addItemToStock(trombone);
+        assertEquals(3, shop.getItemInStockCount());
+        shop.removeItemFromStock(piano);
+        assertEquals(2, shop.getItemInStockCount());
+    }
 }
